58d845596e676a6ff7379318e7a1c5f89a29f20d
anchit mandal <21f1000692@ds.study.iitm.ac.in>
2024-07-11 01:04:36 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        # var3 = request.form
+        var3 = request.get_json()
+        print(var3["data"])
+        # return var3["data"]
+        return jsonify({"message": "got the data!", "data": var3["data"]})
+    if request.method == "GET":
+        var1 = "Hello World from app.py!"
+        # return render_template("main.html", var2=var1)
+        return jsonify({"var2": var1})
+
+if __name__ == "__main__":
+    app.run(debug=True)
