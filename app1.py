be5d89431ef159736a7718e3958af7a3586e237d
anchit mandal <21f1000692@ds.study.iitm.ac.in>
2024-07-12 11:18:07 +0530

    modified:   app1.py 	modified:   instance/test2.sqlite3

--- app1.py
+++ app1.py
@@ -1,6 +1,6 @@
 # Day2: FLask-Security
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_security import Security
 
 from models import db, user_datastore
@@ -9,6 +9,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test2.sqlite3"
 app.config["DEBUG"] = True
 app.config["SECRET_KEY"] = "its_very_secret"
+
 
 db.init_app(app)
 
@@ -22,6 +23,8 @@ with app.app_context():
     db.session.commit()
 
     admin_user = user_datastore.find_user(email="admin@a.com")
+    # admin_user = User.query.filter_by(email="admin@a.com").first()
+    
     if not admin_user:
         # user_datastore.create_user(email="admin@a.com", 
         #                            password="admin", 
@@ -35,11 +38,43 @@ with app.app_context():
         # user_datastore.add_role_to_user(user, "manager")
         # user_datastore.add_role_to_user(user, "customer")
         db.session.commit()
-    user_datastore.add_role_to_user(admin_user, "admin")
-    user_datastore.add_role_to_user(admin_user, "manager")
-    user_datastore.add_role_to_user(admin_user, "customer")
+    if admin_user:
+        user_datastore.add_role_to_user(admin_user, "admin")
+        user_datastore.add_role_to_user(admin_user, "manager")
+        user_datastore.add_role_to_user(admin_user, "customer")
+        db.session.commit()
+        if admin_user.has_role("admin"):
+            print("Admin user already exists!")
+
+
+@app.route("/signup", methods=["POST"])
+def singup():
+    var3 = request.get_json()
+    name1 = var3["email"]
+    desc = var3["password"]
+    username1 = var3["username"] 
+    role = var3["role"]   
+    print(name1, desc, username1)
+    if user_datastore.find_user(email=name1):
+        return jsonify({"message": "User already exists!"}), 403
+    user = user_datastore.create_user(email=name1, 
+                                   password=desc, 
+                                   username=username1)
+    user_datastore.add_role_to_user(user, role)
     db.session.commit()
-    
+    return jsonify({"message": "Data added!", "id": user.id})
+
+@app.route("/signin", methods=["POST"])
+def signin():
+    json = request.get_json()
+    email = json["email"]
+    password = json["password"]
+
+    user = user_datastore.find_user(email=email)
+    if not user:
+        return jsonify({"message": "User not found!"}), 404
+    return jsonify({"message": "User found!"}), 200
+
 
 if __name__ == "__main__":
     app.run() 
